9ce06fe2535f294e94f36314033c20ba0475e83a
odongo <saonarawestinc@outlook.com>
2024-07-30 12:28:01 +0300

    Project Scaffolding

--- frontend/src/components/Layout/Header.jsx
+++ frontend/src/components/Layout/Header.jsx
@@ -1,24 +1,8 @@
 import React from "react";
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  UserButton,
-} from "@clerk/clerk-react";
 
 function Header() {
   return (
-    <header className="w-full flex items-center justify-between">
-      <div className="text-3xl">magical recipes</div>
-      <div className="flex gap-5">
-        {/* <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn> */}
-      </div>
-    </header>
+    <header className="w-full flex items-center justify-between">header</header>
   );
 }
 
