5f07e487268e9ebe9cc428f621857b5f274bef9d
odongo <saonarawestinc@outlook.com>
2024-08-03 16:39:55 +0300

    Added user model and endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,20 @@
 import secrets
-from flask import Flask
-from flask_restful import Api
+from flask import Flask, jsonify
+from flask_restful import Api, Resource
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
+from flask_swagger import swagger
+from flask_swagger_ui import get_swaggerui_blueprint
 from flask_cors import CORS
 from database import db
 
-# import resources
+from resources.user import (
+    RegisterResource,
+    LoginResource,
+    ResetPasswordResource,
+    UserResource,
+    UserListResource,
+)
 
 
 from config import sqliteConfig
@@ -15,18 +23,37 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = sqliteConfig
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["JWT_SECRET_KEY"] = (
-    f"a1d3c56531737{secrets.token_hex(2)}cf62bc36a7e30cd871d7{secrets.token_hex(2)}7b5b51e8208b8cef{secrets.token_hex(2)}c2689e8c0cb412b"
+    f"a1d3c56531737{secrets.token_hex(4)}cf62bc36a7e30cd871d7{secrets.token_hex(4)}7b5b51e8208b8cef{secrets.token_hex(4)}c2689e8c0cb412b"
 )
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = False
 
-api = Api(app)
 db.init_app(app)
+api = Api(app)
 migrate = Migrate(app, db)
 jwt = JWTManager(app)
+swagger = swagger(app)
 
 CORS(app)
 
+
+# auth
+api.add_resource(RegisterResource, "/api/auth/register")
+api.add_resource(LoginResource, "/api/auth/login")
+api.add_resource(ResetPasswordResource, "/auth/reset-password")
+api.add_resource(UserResource, "/api/auth/users/<int:user_id>")
+api.add_resource(UserListResource, "/api/auth/users")
+
+
 # create resources
+# Define a sample resource
+class HelloWorld(Resource):
+    def get(self):
+        return jsonify({"message": "Hello, World!"})
+
+
+# Add the resource to the API
+api.add_resource(HelloWorld, "/hello")
+
 
 with app.app_context():
     db.create_all()
--- backend/models/user.py
+++ backend/models/user.py
@@ -1,50 +1,40 @@
 from werkzeug.security import generate_password_hash, check_password_hash
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
-from sqlalchemy.orm import relationship, validates
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from database import db
+from sqlalchemy.orm import validates
 
 
-class UserRole(Base):
-    __tablename__ = "user_roles"
-
-    id = Column(Integer, primary_key=True)
-    role_name = Column(String)
-
-
-class User(Base):
+class UserModel(db.Model):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
-    firstname = Column(String(80), nullable=False)
-    lastname = Column(String(80), nullable=False)
-    password_hash = Column(String(128), nullable=False)
-    email = Column(String(80), unique=True, nullable=False)
-    is_active = Column(Boolean, default=True)
-    role_id = Column(Integer, ForeignKey("user_roles.id"))
-
-    role = relationship("UserRole")
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(80), nullable=False)
+    lastname = db.Column(db.String(80), nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
 
     @validates("firstname")
     def validate_firstname(self, key, firstname):
         if not firstname:
-            raise ValueError("First name is required.")
+            raise ValueError("First name is required")
         return firstname
 
     @validates("lastname")
     def validate_lastname(self, key, lastname):
         if not lastname:
-            raise ValueError("Last name is required.")
+            raise ValueError("Last name is required")
         return lastname
 
     @validates("email")
     def validate_email(self, key, email):
         if not email:
-            raise ValueError("Email is required.")
-        if User.query.filter_by(email=email).first():
-            raise ValueError("The email is already in use.")
+            raise ValueError("Email is required")
+        if UserModel.query.filter_by(email=email).first():
+            raise ValueError("The email you are trying to use is already in use.")
         return email
+
+    def __repr__(self):
+        return f"<User {self.firstname} {self.lastname} {self.email}>"
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -53,12 +43,66 @@ class User(Base):
         return check_password_hash(self.password_hash, password)
 
     @classmethod
-    def create_user(cls, firstname, lastname, password, email, role_id):
+    def create_user(cls, firstname, lastname, password, email, is_admin=False):
         new_user = cls(
-            firstname=firstname, lastname=lastname, email=email, role_id=role_id
+            firstname=firstname, lastname=lastname, email=email, is_admin=is_admin
         )
         new_user.set_password(password)
+        db.session.add(new_user)
+        db.session.commit()
         return new_user
+
+    @classmethod
+    def get_user(cls, user_id):
+        return cls.query.get(user_id)
+
+    @classmethod
+    def get_user_by_email(cls, email):
+        return cls.query.filter_by(email=email).first()
+
+    @classmethod
+    def get_all_users(cls):
+        return cls.query.all()
+
+    @classmethod
+    def update_user(
+        cls,
+        user_id,
+        firstname=None,
+        lastname=None,
+        password=None,
+        email=None,
+        is_admin=None,
+    ):
+        user = cls.query.get(user_id)
+        if not user:
+            return None
+        if firstname:
+            user.firstname = firstname
+        if lastname:
+            user.lastname = lastname
+        if email:
+            if (
+                cls.query.filter_by(email=email).first()
+                and cls.query.filter_by(email=email).first().id != user_id
+            ):
+                raise ValueError("Email must be unique")
+            user.email = email
+        if password:
+            user.set_password(password)
+        if is_admin is not None:
+            user.is_admin = is_admin
+        db.session.commit()
+        return user
+
+    @classmethod
+    def delete_user(cls, user_id):
+        user = cls.query.get(user_id)
+        if not user:
+            return None
+        db.session.delete(user)
+        db.session.commit()
+        return user
 
     def json(self):
         return {
@@ -66,26 +110,5 @@ class User(Base):
             "firstname": self.firstname,
             "lastname": self.lastname,
             "email": self.email,
-            "is_active": self.is_active,
+            "is_admin": self.is_admin,
         }
-
-
-class Profile(Base):
-    __tablename__ = "profiles"
-
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey("users.id"))
-    bio = Column(String(256))
-    profile_picture_id = Column(Integer, ForeignKey("images.id"))
-
-    user = relationship("User", back_populates="profiles")
-
-
-class Favorite(Base):
-    __tablename__ = "favorites"
-
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey("users.id"))
-    recipe_id = Column(Integer, ForeignKey("recipes.id"))
-
-    user = relationship("User", back_populates="favorites")
