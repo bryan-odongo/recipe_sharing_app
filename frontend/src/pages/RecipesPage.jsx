a73fe8b7123514f177a91fc0844a44082bbdd9ab
odongo <saonarawestinc@outlook.com>
2024-08-05 13:26:28 +0300

    Testing backend APIs

--- frontend/src/pages/RecipesPage.jsx
+++ frontend/src/pages/RecipesPage.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import Layout from "../components/Layout/Layout";
 
 function Recipes() {
-  return <Layout>Recipes</Layout>;
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      const response = await fetch("http://127.0.0.1:5000/api/recipes");
+      const data = await response.json();
+      setRecipes(data);
+      console.log(recipes);
+    };
+    fetchRecipes();
+  }, []);
+
+  return (
+    <Layout>
+      <h1>Recipes</h1>
+      <ul className="grid">
+        {recipes.map((recipe) => (
+          <Link key={recipe.id} to={`/recipes/${recipe?.id}`}>
+            {recipe.title}
+          </Link>
+        ))}
+      </ul>
+    </Layout>
+  );
 }
 
 export default Recipes;
